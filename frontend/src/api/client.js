9099fc800fb1676d8368a1b709634517550d7697
chunix64 <linoxolus@gmail.com>
2026-03-15 12:30:52 +0000

    Prepare for merge to main

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,9 @@
 import config from "@/config";
 
-const api_host = config?.app?.api_host;
+// For dev
+// const api_host = config?.app?.api_host ?? "localhost";
+
+const api_host = config?.app?.api_host ?? "localhost";
 const api_port = config?.app?.api_port;
 const api_base = config?.app?.api_base;
 
